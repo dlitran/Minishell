a85e7a22abf9941cdf66a20fa611f9d010f185c8
dlitran <dariolitran@gmail.com>
2024-02-12 10:09:41 +0100

    again

--- parse_test_mathis/4thparsing/src/ft_dollar_sign.c
+++ parse_test_mathis/4thparsing/src/ft_dollar_sign.c
@@ -6,7 +6,7 @@
 /*   By: mafranco <mafranco@student.barcelona.>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 13:12:42 by mafranco          #+#    #+#             */
-/*   Updated: 2024/01/20 13:13:28 by mafranco         ###   ########.fr       */
+/*   Updated: 2024/02/07 23:44:05 by mafranco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,20 @@
 
 void	ft_dollar_sign(t_data *d)
 {
-	(void)d;
-	printf("dollar sign funcion\n");
+	int	i;
+	int	j;
+
+	i = 1;
+	while (d->cmd->exe[i])
+		i++;
+	j = 0;
+	while (d->env[j])
+	{
+		if (strncmp(d->env[j], &d->cmd->exe[1], i - 1) == 0)
+		{
+			printf("%s\n", &d->env[j][i]);
+			return ;
+		}
+		j++;
+	}	
 }
