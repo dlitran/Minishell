e62bf18858b39cc733404b7779709912f4a942a1
Mafranco31 <mat.francois31@gmail.com>
2024-02-20 16:18:09 +0100

    rpoblemsolved

--- parse_test_mathis/4thparsing/src/utils_execution.c
+++ parse_test_mathis/4thparsing/src/utils_execution.c
@@ -6,7 +6,7 @@
 /*   By: dlitran <dlitran@student.42barcelona.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 17:54:38 by dlitran           #+#    #+#             */
-/*   Updated: 2024/02/20 16:03:06 by mafranco         ###   ########.fr       */
+/*   Updated: 2024/02/20 16:16:40 by mafranco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ static void	*free_error_utils(char *slash, char **matrix, int error)
 		printf("error allocating memory for path\n");
 	return (NULL);
 }
-/*
+
 char	*ft_absolute_path(t_cmd *cmd, t_data *d)
 {
 	char	*joined;
@@ -49,7 +49,7 @@ char	*ft_absolute_path(t_cmd *cmd, t_data *d)
 	joined = ft_strjoin(d->env[i] + 4, slash);
 	free(slash);
 	return (joined);
-}*/
+}
 
 char	*ft_check_path(char *path, t_cmd *cmd, t_data *d)
 {
@@ -58,7 +58,6 @@ char	*ft_check_path(char *path, t_cmd *cmd, t_data *d)
 	char	*slash;
 	char	*joined;
 
-	(void)d;
 	slash = ft_strjoin("/", cmd->exe);
 	if (!slash)
 		return (free_error_utils(NULL, NULL, 1));
@@ -79,16 +78,18 @@ char	*ft_check_path(char *path, t_cmd *cmd, t_data *d)
 		free(joined);
 		i++;
 	}
-/*	free_error_utils(slash, matrix, 0); //We reached the end
+	free_error_utils(slash, matrix, 0); //We reached the end
 	joined = ft_absolute_path(cmd, d);
 	if(!access(joined, F_OK))
 		return (joined);
 	free(joined);
-	if (ft_strncmp(cmd->exe, "./", 2))
+	if (ft_strncmp(cmd->exe, "./", 2) == 0)
+	{
 		joined = ft_path(d);
-	if (!access(joined, F_OK))
-		return (joined);
-	free(joined);*/
+		if (!access(joined, F_OK))
+			return (joined);
+		free(joined);
+	}
 	printf("command not found: %s\n", cmd->exe);
 	return (NULL);
 }
