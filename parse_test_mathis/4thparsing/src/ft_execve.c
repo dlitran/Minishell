2bd84aaeef092fd65500a429b4583239df7aadb7
Mafranco31 <mat.francois31@gmail.com>
2024-03-09 19:02:29 +0100

    execve

--- parse_test_mathis/4thparsing/src/ft_execve.c
+++ parse_test_mathis/4thparsing/src/ft_execve.c
@@ -6,29 +6,39 @@
 /*   By: dlitran <dlitran@student.42barcelona.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 13:22:31 by mafranco          #+#    #+#             */
-/*   Updated: 2024/03/07 08:17:28 by dlitran          ###   ########.fr       */
+/*   Updated: 2024/03/09 19:01:56 by mafranco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header.h"
 
-void	ft_execve(t_data *d, char *path)
+static char	*exec_path(t_data *d, char *path)
 {
-	pid_t	pid;
-
 	if (d->cmd->exe && d->cmd->exe[0] == 47 && !access(d->cmd->exe, F_OK))
 	{
 		path = ft_strdup(d->cmd->exe);
 		if (!path)
-			return (v_err_msg("error allocating memory for ft_execve\n", 68));
+			return (c_err_msg("error allocating memory for ft_execve\n", 68));
 	}
 	else
 	{
 		path = ft_check_path(d->path, d->cmd, d, 0);
 		if (!path)
-			return ;
+			return (NULL);
 	}
+	return (path);
+}
+
+void	ft_execve(t_data *d, char *path)
+{
+	pid_t	pid;
+
+	path = exec_path(d, path);
+	if (!path)
+		return ;
 	pid = fork();
+	if (pid == -1)
+		return (v_err_msg("error fork\n", 69));
 	if (pid == 0)
 	{
 		if (execve(path, d->cmd->arg, d->env) == -1)
